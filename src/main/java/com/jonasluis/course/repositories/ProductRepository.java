618845535b876f2c63cdb055bd0ea3e629c42124
Jonas Luis <jonasluis66@gmail.com>
2024-05-27 16:15:22 -0300

    Product

---
+++ src/main/java/com/jonasluis/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.jonasluis.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jonasluis.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	
+}
